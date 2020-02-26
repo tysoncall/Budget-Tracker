bcea8d2621d6b746d5f8c0c3318c22c4a7dd1e0c
Tyson Call <Tyson@Savannahs-MacBook-Air.local>
2020-02-25 20:14:49 -0700

    Finished, just need deployed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Budget-Tracker
+
+#User Story
+AS AN avid traveller
+I WANT to be able to track my withdrawals and deposits with or without a data/internet connection
+SO THAT my account balance is accurate when I am traveling
+
+Basic budget tracker that can be used online OR offline built with basic express and mongoose.
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3001;
 
 const app = express();
 
-// Server
+// Server //
 app.use(logger("dev"));
 
 app.use(compression());
@@ -20,7 +20,7 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// Routes
+// Routes //
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
