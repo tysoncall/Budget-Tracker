bad0f6d14a9b49745ea92e08da9a5443d8da4943
Tyson Call <Tyson@Savannahs-MacBook-Air.local>
2020-02-27 19:17:43 -0700

    Heroku deploy attempt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,6 +19,10 @@ mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget");
+
+
 
 // Routes //
 app.use(require("./routes/api.js"));
